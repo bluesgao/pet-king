71a2699539be229ea650d690cf917b099a88c1b4
gaoxin11 <gaoxin11@jd.com>
2019-12-25 11:24:40 +0800

    video-item

---
+++ components/video-item/video-item.js
@@ -0,0 +1,26 @@
+Component({
+  data: {},
+  properties: {
+    video: {
+      type: Object,
+      value: {},
+    }
+  },
+  methods: {
+    //跳转详情页
+    gotoDetail(e){
+      wx.navigateTo({
+        url: '/pages/post',
+        success: function(res){
+          // success
+        },
+        fail: function() {
+          // fail
+        },
+        complete: function() {
+          // complete
+        }
+      })
+    }
+  }
+})
