bb87d84e7e1cfe48121ffa8b388cfe9976b73412
bluesgao <bluesgao@126.com>
2019-12-26 18:08:55 +0800

    searchBar

--- components/searchBar/searchBar.js
+++ components/searchBar/searchBar.js
@@ -5,9 +5,9 @@ Component({
     properties: {
         content: {//搜索内容
             type: String,
-            value: '搜索内容'
+            value: ''
         },
-        flag: {//是否显示clear按钮
+        clearFlag: {//是否显示clear按钮
             type: Boolean,
             value: false,
         }
@@ -30,9 +30,6 @@ Component({
         //获得焦点
         focus() {
             console.info("获得焦点");
-            this.setData({
-                flag: true,
-            })
         },
         //失去焦点
         blur() {
@@ -45,6 +42,18 @@ Component({
                 content: e.detail.value,
             })
             console.info('搜索输入 content', this.data.content)
+
+            //如果输入数据长度不为空，则显示清除按钮
+            //反之，不显示清除按钮
+            if(this.data.content!=''){
+                this.setData({
+                    clearFlag: true,
+                })
+            }else{
+                this.setData({
+                    clearFlag: false,
+                })
+            }
             this.triggerEvent("searchList", e);
         },
         //查询
@@ -55,6 +64,13 @@ Component({
         //清空搜索框
         clear(e) {
             console.info("清空搜索框",e);
+            console.info('清空搜索框前 content', this.data.content)
+            this.setData({
+                content: '',
+                clearFlag:false,
+            })
+            console.info('清空搜索框后 content', this.data.content)
+
             this.triggerEvent("clearSearch");
         },
         // 取消
